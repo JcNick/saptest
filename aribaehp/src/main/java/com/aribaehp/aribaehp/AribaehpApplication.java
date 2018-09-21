5b5483ad88946b46e0e1c1099fe3e689c0006999
nickjc <hejunxingmn@126.com>
2018-09-21 15:27:48 +0800

    first commit

---
+++ aribaehp/src/main/java/com/aribaehp/aribaehp/AribaehpApplication.java
@@ -0,0 +1,12 @@
+package com.aribaehp.aribaehp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AribaehpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AribaehpApplication.class, args);
+	}
+}
